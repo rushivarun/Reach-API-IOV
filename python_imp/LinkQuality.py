c5d91e463d6ccf48eb8d3d9535e771466e111bb9
Rushi Varun <rushi2000varun@gmail.com>
2020-04-13 13:35:09 +0530

    signal achieved

--- python_imp/LinkQuality.py
+++ python_imp/LinkQuality.py
@@ -4,7 +4,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Display WLAN signal strength.')
 parser.add_argument(dest='interface', nargs='?', default='wlp2s0',
-                    help='wlan interface (default: wlan0)')
+                    help='wlan interface (default: wlp2s0)')
 args = parser.parse_args()
 
 print('\n---Press CTRL+Z or CTRL+C to stop.---\n')
@@ -13,8 +13,9 @@ while True:
     cmd = subprocess.Popen('iwconfig %s' % args.interface, shell=True,
                            stdout=subprocess.PIPE)
     for line in cmd.stdout:
-        if 'Link Quality' in line:
-            print(line.lstrip(' '),)
-        elif 'Not-Associated' in line:
+        # print(line)
+        if b'Link Quality' in line:
+            print(line.lstrip(b' '),)
+        elif b'Not-Associated' in line:
             print('No signal')
     time.sleep(1)
